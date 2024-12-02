f7a0b316209526b150c9d38693eaabda0d0b4b21
Topskys <2477350343@qq.com>
2024-12-02 21:00:16 +0800

    feat: 动态路由案例及提升样式优先级

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { Card } from "antd";
+import React from "react";
+import { data } from "@/data";
 
-export default function page() {
+export default function page({ params }: { params: { id: string } }) {
+  const item = data.find((v) => v.id == +params.id)!;
   return (
-    <div>page</div>
-  )
+    <Card
+      title={item?.title}
+      extra={''}
+    >
+      <p>{item.body}</p>
+    </Card>
+  );
 }
