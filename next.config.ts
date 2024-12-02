ae617c911f328af725eae63e568e0e57b4d8d000
Topskys <2477350343@qq.com>
2024-12-02 11:43:38 +0800

    feat: 关于usePathname

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true, // 严格模式，默认开启
 };
 
 export default nextConfig;
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,12 @@
 "use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React, { useState } from "react";
+
+const links = [
+  { name: "About", href: "/dashboard/about" },
+  { name: "Settings", href: "/dashboard/settings" },
+];
 
 export default function DashboardLayout({
   children,
@@ -12,12 +18,20 @@ export default function DashboardLayout({
    * 解决：在该文件头部添加 "use client" 变为CSR即可使用
    */
   const [count, setCount] = useState(0);
+  const pathname = usePathname(); // /dashboard/about
 
   return (
     <div className="border-2 border-dashed border-black p-4 w-1/2 max-auto mt-4">
-      <div className="flex gap-4 font-bold text-lg mb-4 text-purple-500">
-        <Link href="/dashboard/about">About</Link>
-        <Link href="/dashboard/settings">Settings</Link>
+      <div className="flex gap-4 font-bold text-lg mb-4">
+        {links.map(({ name, href }) => (
+          <Link
+            key={name}
+            className={pathname === href ? "text-purple-500" : ""}
+            href={href}
+          >
+            {name}
+          </Link>
+        ))}
       </div>
       <div>Dashboard Layout {count}</div>
       <button
