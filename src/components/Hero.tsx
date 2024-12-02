909e3d97f45224c74f0a422d225c98fb42c482d3
Topskys <2477350343@qq.com>
2024-12-02 14:01:14 +0800

    fix: 修复通用图片背景文字展示

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ export default function Page(props: IProps) {
   const { alt = "image", imgUrl, content } = props;
   return (
     <div className="h-screen relative">
-      <div className="absolute inset-0">
+      <div className="absolute inset-0 -z-10">
         <Image
           src={imgUrl}
           fill
@@ -21,7 +21,7 @@ export default function Page(props: IProps) {
         <div className="absolute inset-0 bg-gradient-to-r from-gray-900"></div>
       </div>
       <div className="flex justify-center pt-48">
-        <h1 className="text-white">{content}</h1>
+        <h1 className="text-white text-6xl">{content}</h1>
       </div>
     </div>
   );
